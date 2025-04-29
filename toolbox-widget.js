320830f3e1de2249149c778790a9d16044a3bbfa
Vijaya Kumar <vijaya.kumar.ex@geidea.net>
2025-04-29 17:55:46 +0530

    Background color updated

--- toolbox-widget.js
+++ toolbox-widget.js
@@ -6,6 +6,13 @@
   document.body.appendChild(widgetRoot);
 
   const shadow = widgetRoot.attachShadow({ mode: "open" });
+   // Inject CSS into Shadow DOM
+   const cssLink = document.createElement("link");
+   cssLink.rel = "stylesheet";
+   cssLink.type = "text/css";
+   // cssLink.href = "https://willowy-cuchufli-cb1231.netlify.app/react-toolbox-widget.bundle.css";
+   cssLink.href = "https://rictestapp.netlify.app/react-toolbox-widget.bundle.css";
+   shadow.appendChild(cssLink);
 
   // Container to hold the app
   const container = document.createElement("div");
@@ -18,32 +25,29 @@
   container.style.zIndex = "99999";
   shadow.appendChild(container);
 
-  // Inject CSS into Shadow DOM
-  const cssLink = document.createElement("link");
-  cssLink.rel = "stylesheet";
-  cssLink.type = "text/css";
-  cssLink.href = "https://rictestapp.netlify.app/react-toolbox-widget.bundle.css";
-  container.appendChild(cssLink);
+  
 
   // Create container for React app
   const appContainer = document.createElement("div");
   appContainer.id = "toolbox-app";
+  appContainer.style.backgroundColor = "white";
   appContainer.style.width = "100%";
   container.appendChild(appContainer);
 
   // Load React widget script immediately
   const script = document.createElement("script");
+  // script.src = "https://willowy-cuchufli-cb1231.netlify.app/react-toolbox-widget.bundle.iife.js";
   script.src = "https://rictestapp.netlify.app/react-toolbox-widget.bundle.iife.js";
   script.onload = () => {
-    console.log("✅ Widget script loaded");
+    console.log("Widget script loaded");
     if (typeof window.renderToolboxWidget === "function") {
       window.renderToolboxWidget(appContainer, { customerId });
     } else {
-      console.error("❌ renderToolboxWidget not available on window.");
+      console.error("renderToolboxWidget not available on window.");
     }
   };
   script.onerror = (err) => {
-    console.error("❌ Failed to load widget script", err);
+    console.error("Failed to load widget script", err);
   };
   container.appendChild(script);
 })();
